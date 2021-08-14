080efebfc4d0205f2cf36390e5618d41fe2dbfd2
therexone <renerjake@gmail.com>
2021-08-14 21:36:17 +0530

    feat: setup next-pwa, add react-query, styled-components

---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <meta name="application-name" content="r/Cricket live thread stream" />
+        <meta name="description" content="r/Cricket live thread stream" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/static/manifest.json" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/_offline.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const OfflinePage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>stream r/Cricket | Offline</title>
+        <meta name="description" content="Live r/Cricket threads" />
+      </Head>
+      streamRcricket | Offline
+    </div>
+  );
+};
+
+export default OfflinePage;
